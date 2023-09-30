b8d14342fe66c238d1311a1fad0c5444431fc3c0
rgap <r.guzmanap@gmail.com>
2023-09-29 23:00:25 -0500

    quick update

--- react-tip-calculator/src/App.jsx
+++ react-tip-calculator/src/App.jsx
@@ -1,29 +1,152 @@
-import { Card, Header, ItemPercentage, Summary, TextField, Title } from "./components";
+import { useState } from "react";
+import {
+  Card,
+  Header,
+  ItemPercentage,
+  Summary,
+  TextField,
+  Title,
+} from "./components";
 
 export default function App() {
+  const [percentages, setPercetanges] = useState([
+    {
+      value: "5%",
+      isActive: false,
+    },
+    {
+      value: "10%",
+      isActive: false,
+    },
+    {
+      value: "15%",
+      isActive: false,
+    },
+    {
+      value: "25%",
+      isActive: false,
+    },
+    {
+      value: "50%",
+      isActive: false,
+    },
+  ]);
 
-  const percentages = ["5%", "10%", "15%", "25%", "50%"]
+  const [customValue, setCustomValue] = useState("");
+  const [inputBill, setInputBill] = useState("");
+  const [inputPeople, setInputPeople] = useState("");
+  const [valuePercetange, setValuePercetange] = useState(0);
+
+  // const handleInputBillOnChange = (event) => {
+  //   setInputBill(event.target.value);
+  // };
+  // const handleInputPeopleOnChange = (event) => {
+  //   setInputPeople(event.target.value);
+  // };
+
+  // const handlePercetangeActive = (value) => {
+  //   const item = percentages.map((percentage) => {
+  //     return {
+  //       isActive: value && !percentage.isActive && percentage.value === value,
+  //       value: percentage.value,
+  //     };
+  //   });
+  //   setPercetanges(item);
+  //   setCustomValue("");
+  // };
+
+  // const handleInputOnChange = (event) => {
+  //   const item = percentages.map((percentage) => {
+  //     return {
+  //       isActive: false,
+  //       value: percentage.value,
+  //     };
+  //   });
+  //   setPercetanges(item);
+
+  //   setCustomValue(event.target.value);
+  // };
+
+  const handleMapPercentage = (value) => {
+    return percentages.map((percentage) => {
+      return {
+        isActive: value && !percentage.isActive && percentage.value === value,
+        value: percentage.value,
+      };
+    });
+  };
+
+  const handlePercetangeActive = (value) => {
+    setPercetanges(handleMapPercentage(value));
+    setCustomValue("");
+    setValuePercetange(Number(value.replace("%", "")));
+    // console.log(Number(value.replace("%", "")));
+  };
+
+  const handleInputOnChange = (event) => {
+    setPercetanges(handleMapPercentage());
+    setCustomValue(event.target.value);
+    setValuePercetange(Number(event.target.value));
+  };
+
+  const handleInputBillOnChange = (event) => setInputBill(event.target.value);
+
+  const handleInputPeopleOnChange = (event) => {
+    if (!valuePercetange || !inputBill) {
+      alert("Debes completar los valores para el calculo");
+      return;
+    }
+
+    const inputPeople = event.target.value;
+    setInputPeople(inputPeople);
+
+    // calculos
+    const bill = Number(inputBill);
+
+    const totalTipPerPerson = (bill * valuePercetange) / Number(inputPeople);
+    console.log(totalTipPerPerson);
+    const totalPerPerson = bill / Number(inputPeople) + totalTipPerPerson;
+    console.log(totalPerPerson);
+  };
+
   return (
     <main className="bg-green-100 h-screen">
       <Header />
       <Card>
-        <Title text="Bill"/>
-        <TextField icon="dollar" placeholder="0.0" name="input-dollar" />
-        <Title text="Select Tip %"/>
+        <Title text="Bill" />
+        <TextField
+          icon="dollar"
+          placeholder="0.0"
+          name="input-dollar"
+          value={inputBill}
+          onChange={handleInputBillOnChange}
+        />
+        <Title text="Select Tip %" />
         <div className="grid grid-cols-2 gap-3 mt-3">
-          {/* <ItemPercentage value="5%" />
-          <ItemPercentage value="10%" />
-          <ItemPercentage value="15%" />
-          <ItemPercentage value="25%" />
-          <ItemPercentage value="50%" /> */}
           {percentages.map((percentage) => (
-            <ItemPercentage key={percentage} value={percentage}/>)) 
-          }
-          <TextField placeholder="Custom" name="input-custom"/>
+            <ItemPercentage
+              key={percentage.value}
+              value={percentage.value}
+              isActive={percentage.isActive}
+              onElementClick={handlePercetangeActive}
+            />
+          ))}
+          <TextField
+            placeholder="Custom"
+            name="input-custom"
+            value={customValue}
+            onChange={handleInputOnChange}
+          />
         </div>
-        <Title text="Number of people"/>
-        <TextField icon="person" placeholder="0" name="input-people"/>
-        <Summary/>
+        <Title text="Number of people" />
+        <TextField
+          icon="person"
+          placeholder="0"
+          name="input-people"
+          value={inputPeople}
+          onChange={handleInputPeopleOnChange}
+        />
+        <Summary />
       </Card>
     </main>
   );
@@ -41,3 +164,9 @@ export default function App() {
 // when iterating there has to be a key
 
 // astro build: para crear interfaces de forma dinamica
+
+/* <ItemPercentage value="5%" />
+          <ItemPercentage value="10%" />
+          <ItemPercentage value="15%" />
+          <ItemPercentage value="25%" />
+          <ItemPercentage value="50%" /> */
--- react-tip-calculator/src/components/TextField/index.jsx
+++ react-tip-calculator/src/components/TextField/index.jsx
@@ -1,21 +1,46 @@
+import { useRef } from "react";
 import dollarIcon from "../../assets/icon-dollar.svg";
 import personIcon from "../../assets/icon-person.svg";
 
-export default function TextField({ icon, name, placeholder }) {
+export default function TextField({
+  icon,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) {
+  const inputRef = useRef(null);
+
   const icons = {
     dollar: dollarIcon,
     person: personIcon,
   };
 
+  const handleInputRef = () => {
+    // console.log(inputRef);
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="flex items-center p-3 rounded-md bg-green-100/50 cursor-text">
+    <div
+      onClick={handleInputRef}
+      className="flex items-center p-3 rounded-md bg-green-100/50 cursor-text"
+    >
       <img src={icons[icon]} alt="" />
       <input
-        type="text"
+        ref={inputRef}
+        type="number"
         name={name}
+        value={value}
         placeholder={placeholder}
+        onChange={onChange}
         className="w-full text-right outline-none bg-transparent text-green-900 text-xl font-semibold"
       />
     </div>
   );
 }
+
+// todo elemento en react tiene un r ef
+
+// atribute={prop}
+// ex: onChange={onChange}
