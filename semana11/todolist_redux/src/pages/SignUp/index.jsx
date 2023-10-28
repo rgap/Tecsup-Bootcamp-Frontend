fc0ab7139c6bb704b025a4cd9632f4854a0c749e
Rel Guzman <r.guzmanap@gmail.com>
2023-10-27 22:40:01 -0500

    quick update

--- semana11/todolist_redux/src/pages/SignUp/index.jsx
+++ semana11/todolist_redux/src/pages/SignUp/index.jsx
@@ -30,6 +30,7 @@ export default function SignUp() {
     <div className="h-screen flex items-center justify-center max-w-md mx-auto">
       <Card>
         <h1 className="text-2xl font-semibold my-5">Crear Cuenta</h1>
+        <span>Crea tu cuenta 👇</span>
         <Form
           inputs={inputs}
           errors={errors}
